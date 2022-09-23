3a5557d25b280a435bbce0efb969a31638aa73b3
mseshiAyanda <mseshidladla@gmail.com>
2022-09-23 12:42:38 +0200

    template

--- Student_management/students/models.py
+++ Student_management/students/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    studenty_number = models.PositiveIntegerField()
-    first_name = models.CharField(max_length=500)
-    last_name = models.CharField(max_length=500)
-    email = models.EmailField(max_length=100)
-    field_of_study = models.CharField(max_length=100)
-    gpa = models.FloatField()
-    
-    def __str__(self):
-        return f'Student: {self.first_name} {self.last_name}'
+  student_number = models.PositiveIntegerField()
+  first_name = models.CharField(max_length=50)
+  last_name = models.CharField(max_length=50)
+  email = models.EmailField(max_length=100)
+  field_of_study = models.CharField(max_length=50)
+  gpa = models.FloatField()
+
+  def __str__(self):
+    return f'Student: {self.first_name} {self.last_name}'
--- Student_management/students/views.py
+++ Student_management/students/views.py
@@ -7,7 +7,7 @@ from .forms import StudentForm
 
 # Create your views here.
 def index(request):
-  return render(request, 'students/index.html', {
+  return render(request, 'index.html', {
     'students': Student.objects.all()
   })
 
@@ -35,13 +35,13 @@ def add(request):
         gpa = new_gpa
       )
       new_student.save()
-      return render(request, 'students/add.html', {
+      return render(request, 'add.html', {
         'form': StudentForm(),
         'success': True
       })
   else:
     form = StudentForm()
-  return render(request, 'students/add.html', {
+  return render(request, 'add.html', {
     'form': StudentForm()
   })
 
@@ -51,14 +51,14 @@ def edit(request, id):
     form = StudentForm(request.POST, instance=student)
     if form.is_valid():
       form.save()
-      return render(request, 'students/edit.html', {
+      return render(request, 'edit.html', {
         'form': form,
         'success': True
       })
   else:
     student = Student.objects.get(pk=id)
     form = StudentForm(instance=student)
-  return render(request, 'students/edit.html', {
+  return render(request, 'edit.html', {
     'form': form
   })
 
