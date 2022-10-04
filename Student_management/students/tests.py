636d812d6c3c21ce92b106d982a05f41a681b562
mseshiAyanda <mseshidladla@gmail.com>
2022-10-04 17:37:02 +0200

    commit

--- Student_management/students/tests.py
+++ Student_management/students/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+from .models import Student
+
+class modelTest(TestCase):
+    def Create_Data(self, student_number=int(234566), first_name="ayanda", last_name="msomi", field_of_study="IT", gpa=2.6):
+        return Student.objects.create(student_number=student_number, first_name=first_name, last_name=last_name, field_of_study=field_of_study, gpa=gpa)
+    
+    def test_Data(self):
+        d = self.Create_Data()
+        self.assertTrue(isinstance(d, Student))
+
+
+c = Client()
+response = c.post('/login/', {'username': 'ayanda', 'password': 'sdfghjhgfd!'})
+response.status_code, 200
+response = c.get
+       
+
