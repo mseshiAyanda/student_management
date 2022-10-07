38a718fe7b690bcc7a47daad2e8f4818abece1b6
mseshiAyanda <mseshidladla@gmail.com>
2022-10-07 21:37:20 +0200

    environ

--- Student_management/Student_management/settings.py
+++ Student_management/Student_management/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1=!3)2w3d*e#k@k&&u28&!=&xlxkz)xf4z404a4mwj5-+snl6r'
+SECRET_KEY = os.environ.get('DJANGO_SEKRETE_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
